773eaf3d65dba54fea281b5f88aeae08545467eb
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-06-19 13:10:01 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,6 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
