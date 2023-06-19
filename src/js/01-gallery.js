9cbad0d5d4e4d5e2cd588556db20a46c61a7440d
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-06-19 13:31:12 +0300

    task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,3 +6,30 @@ console.log(galleryItems);
 
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({preview, original, description})=>`<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+    />
+    </a>
+    </li>`
+).join('');
+
+// console.log(markup);
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox('.gallery a', { 
+    captionSelector: 'img',
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250, 
+    download: false
+});
