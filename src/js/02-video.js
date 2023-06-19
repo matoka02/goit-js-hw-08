686214f25443648b792ec26728ff76fdce1a9a53
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-06-19 17:12:17 +0300

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(evt) {
+    localStorage.setItem(LOCALSTORAGE_KEY, evt.seconds);
+};
+
+player
+.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY))
+.catch(function (error) {
+    console.error(error);
+})
